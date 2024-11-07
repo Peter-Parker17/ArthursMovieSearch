3e9b8514bc8aac759e107bd21d5ef32c01d0e03e
Arthur <menacejuco1163@yahoo.com>
2024-11-07 15:31:49 -0800

    updated

---
+++ api.js
@@ -0,0 +1,33 @@
+const movieTitle = document.querySelector(".search");
+const resultEl = document.querySelector(".movies");
+
+
+async function searchMovie(movieFilter) {
+    event.preventDefault()
+    const movieTitles = movieTitle.value
+  const data = await fetch(
+    `https://www.omdbapi.com/?i=tt3896198&apikey=b5cd70c4&s=${movieTitles || ""}`
+  );
+  const movieData = await data.json();
+  const result = movieData.Search
+  if (movieFilter === "newtoold") {
+    result.sort((a,b) => b.Year- a.Year)
+  }
+  if (movieFilter === "oldtonew") {
+    result.sort((a,b) => a.Year- b.Year)
+  }
+  console.log(result);
+  resultEl.innerHTML = movieData.Search.map(item => (
+    `<div>
+                <img src="${item.Poster}" alt="">
+                <h2>${item.Title}</h2>
+                <h3>${item.Type}</h3>
+                <h4>${item.Year}</h4>
+            </div>
+            `
+  ))
+}
+
+function yearFilter() {
+    searchMovie(event.target.value)
+}
